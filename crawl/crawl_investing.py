2cc7d8afba448595bfc74370e006c786c54a6ade
hopark <pch4463@gmail.com>
2020-03-18 23:12:20 +0900

    fix error

--- crawl/crawl_investing.py
+++ crawl/crawl_investing.py
@@ -8,7 +8,7 @@ import json
 
 url = 'https://kr.investing.com/stock-screener/Service/SearchStocks'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36", "x-Requested-With": "XMLHttpRequest"}
-data = {
+param = {
     "country[]": "11",
     "sector": "3,1,7,6,12,2,8,9,10,11,5,4",
     "industry": "18,53,67,89,43,31,6,38,87,77,66,81,48,16,11,54,33,24,20,29,91,36,73,21,50,3,63,7,10,86,78,101,27,90,85,82,2,96,8,22,14,45,92,65,40,70,42,39,99,98,79,64,80,15,26,44,74,97,76,88,17,12,47,62,68,83,84,57,35,72,51,25,28,5,60,19,4,61,37,34,93,71,30,102,100,58,95,94,32,13,46,1,52,23,75,56,59,41,49,55,69,9",
@@ -30,7 +30,7 @@ with requests.Session() as s:
     pages = int(int(sys.argv[1])/50)
     num = 0
     for pn in range(pages):
-        stock = s.post(url, headers=headers, verify=False, data={**data, "pn": str(pn+1)}).json()['hits']
+        stock = s.post(url, headers=headers, verify=False, data={**param, "pn": str(pn+1)}).json()['hits']
         for i in range(len(stock)):
             balance_sheet = getBalanceSheet(stock[i]['pair_ID'])
             rows = balance_sheet.select('tr.pointer')
