0e7b1155fd3fe854d9acbfa3660ecd6410c047a2
Changho Park <pch4463@gmail.com>
2020-04-18 05:07:05 +0900

    change into naver finance.

--- modules/constant.py
+++ modules/constant.py
@@ -1,22 +1,24 @@
-# IP
-SUWON_IP = '168.219.61.252:8080'
-SEOUL_IP = '10.112.1.184:8080'
+from enum import Enum
 
-# CRAWL
-SEARCH_VIEW_URL = 'https://kr.investing.com/stock-screener'
-SEARCH_URL = 'https://kr.investing.com/stock-screener/Service/SearchStocks'
-PROFIT_URL = 'https://kr.investing.com/instruments/Financials/changesummaryreporttypeajax?action=change_report_type&pid={stock_id}&financial_id={stock_id}&ratios_id={stock_id}&period_type=Annual'
-BALANCE_SHEET_URL = 'https://kr.investing.com/instruments/Financials/changereporttypeajax?action=change_report_type&pair_ID={stock_id}&report_type=BAL&period_type=Annual'
-SEARCH_HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-                 "x-Requested-With": "XMLHttpRequest"}
-KOR_STOCK_PARAM = {"country[]": "11",
-                   "sector": "3,1,7,6,12,2,8,9,10,11,5,4",
-                   "industry": "18,53,67,89,43,31,6,38,87,77,66,81,48,16,11,54,33,24,20,29,91,36,73,21,50,3,63,7,10,86,78,101,27,90,85,82,2,96,8,22,14,45,92,65,40,70,42,39,99,98,79,64,80,15,26,44,74,97,76,88,17,12,47,62,68,83,84,57,35,72,51,25,28,5,60,19,4,61,37,34,93,71,30,102,100,58,95,94,32,13,46,1,52,23,75,56,59,41,49,55,69,9",
-                   "equityType": "ORD,DRC,Preferred,Unit,ClosedEnd,REIT,ELKS,OpenEnd,Right,ParticipationShare,CapitalSecurity,PerpetualCapitalSecurity,GuaranteeCertificate,IGC,Warrant,SeniorNote,Debenture,ETF,ADR,ETC,ETN",
-                   "exchange[]": ["130", "110", "60"],
-                   "order[col]": "eq_market_cap",
-                   "order[dir]": "d"}
-STOCK_PER_PAGE = 50
-GET_ALL_STOCK = -1
-
-OUTPUT_CSV = 'stock_info.csv'
+class CONST:
+    class IP:
+        SUWON = '168.219.61.252:8080'
+        SEOUL = '10.112.1.184:8080'
+    class Url:
+        STOCK_LIST = 'https://www.ktb.co.kr/trading/popup/itemPop.jspx'
+        STOCK_INFO = 'https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd={stock_code}&cn='
+        STOCK_SHEET = 'https://navercomp.wisereport.co.kr/v2/company/cF3002.aspx?cmp_cd={stock_code}&frq=0&rpt={rpt}&frqTyp=0&finGubun=MAIN&cn=&encparam={encparam}'
+        SUMMARY = 'https://navercomp.wisereport.co.kr/v2/company/cF1001.aspx?cmp_cd={stock_code}&fin_typ=0&freq_typ=A&encparam={encparam}'
+    class InfoType(Enum):
+        PROFIT = 0
+        BALANCE = 1
+    class StockType(Enum):
+        ALL_STOCK = 0
+        CODE = 1
+        NAME = 2
+    class Year:
+        LAST = 'DATA4'
+        THIS = 'DATA5'
+        NEXT = 'DATA6'
+    class Num:
+        VALUE_CONST = 9.09
